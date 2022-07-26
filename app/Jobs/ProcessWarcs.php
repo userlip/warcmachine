f798762e24dc3432a42cbeb5e55fd3896ce8e2ad
The F <marindelija@gmail.com>
2022-07-26 03:32:19 +0200

    log errors

--- app/Jobs/ProcessWarcs.php
+++ app/Jobs/ProcessWarcs.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Mixnode\WarcReader;
 
@@ -48,20 +49,26 @@ class ProcessWarcs implements ShouldQueue
         // The WarcReader constructure accepts paths to both raw WARC files and GZipped WARC files
         $warc_reader = new WarcReader('/warc/' . $basename . $time . '.warc.gz');
 
-        // Using nextRecord, iterate through the WARC file and output each record.
-        while (($record = $warc_reader->nextRecord()) != FALSE) {
+        try {
+            // Using nextRecord, iterate through the WARC file and output each record.
+            while (($record = $warc_reader->nextRecord()) != FALSE) {
 
-            if (!isset($record['header']['WARC-Target-URI'])) {
-                continue;
+                if (!isset($record['header']['WARC-Target-URI'])) {
+                    continue;
+                }
+
+                // Use your own regex here!
+                preg_match_all('/[\'\"\s][a-z0-9._-]{0,35}@[a-z0-9._-]+\.[a-z]{2,4}/mi', $record['content'], $matches);
+
+                foreach ($matches[0] as $match) {
+                    $data[] = array($match, $record['header']['WARC-Target-URI']);
+                }
             }
-
-            // Use your own regex here!
-            preg_match_all('/[\'\"\s][a-z0-9._-]{0,35}@[a-z0-9._-]+\.[a-z]{2,4}/mi', $record['content'], $matches);
-
-            foreach ($matches[0] as $match) {
-                $data[] = array($match, $record['header']['WARC-Target-URI']);
-            }
+        } catch (\Throwable $th) {
+            Log::error($th);
         }
+
+
 
         // Or upload to S3
         // https://laravel.com/docs/9.x/filesystem#amazon-s3-compatible-filesystems
