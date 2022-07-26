6f8b616ef0262c02ab7f5e73d85cb82cabe944f5
The F <marindelija@gmail.com>
2022-07-26 02:41:31 +0200

    rollback to mixnode warc and try to debug it

--- app/Jobs/ProcessWarcs.php
+++ app/Jobs/ProcessWarcs.php
@@ -44,38 +44,28 @@ class ProcessWarcs implements ShouldQueue
 
         exec("aria2c -s16 -x16 -o ./../../../warc/{$basename}{$time}.warc.gz " . trim(str_replace('\r\n', '', $this->url)));
 
-        ////////////////////////////////////////////////////////////////////////////////
-        //
-        // Having some issues with the WARC Reader by Mixnode, so using 
-        // https://github.com/c6fc/warcannon/blob/dev/lambda_functions/warcannon/parse_regex.js 
-        // code from here!
-        //
-        ///////////////////////////////////////////////////////////////////////////////
-
-        $data = exec('/usr/bin/node /root/warcmachine/warc.js /warc/' . $basename . $time . '.warc.gz');
-
         // Initialize a WarcReader object 
         // The WarcReader constructure accepts paths to both raw WARC files and GZipped WARC files
-        // $warc_reader = new WarcReader('/warc/' . $basename . $time . '.warc.gz');
+        $warc_reader = new WarcReader('/warc/' . $basename . $time . '.warc.gz');
 
-        // // Using nextRecord, iterate through the WARC file and output each record.
-        // while (($record = $warc_reader->nextRecord()) != FALSE) {
+        // Using nextRecord, iterate through the WARC file and output each record.
+        while (($record = $warc_reader->nextRecord()) != FALSE) {
 
-        //     if (!isset($record['header']['WARC-Target-URI'])) {
-        //         continue;
-        //     }
+            if (!isset($record['header']['WARC-Target-URI'])) {
+                continue;
+            }
 
-        //     // Use your own regex here!
-        //     preg_match_all('/[\'\"\s][a-z0-9._-]{0,35}@[a-z0-9._-]+\.[a-z]{2,4}/mi', $record['content'], $matches);
+            // Use your own regex here!
+            preg_match_all('/[\'\"\s][a-z0-9._-]{0,35}@[a-z0-9._-]+\.[a-z]{2,4}/mi', $record['content'], $matches);
 
-        //     foreach ($matches[0] as $match) {
-        //         $data[] = array($match, $record['header']['WARC-Target-URI']);
-        //     }
-        // }
+            foreach ($matches[0] as $match) {
+                $data[] = array($match, $record['header']['WARC-Target-URI']);
+            }
+        }
 
         // Or upload to S3
         // https://laravel.com/docs/9.x/filesystem#amazon-s3-compatible-filesystems
-        Storage::disk('sftp')->put($basename . $time . '.txt', $data);
+        Storage::disk('sftp')->put($basename . $time . '.txt', json_encode($data));
         unlink('/warc/' . $basename . $time . '.warc.gz');
     }
 }
